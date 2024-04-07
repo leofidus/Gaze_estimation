c176212f2947a5a7cc0a7c78c4fbc6a34a90f18d
Jannis Froese <jannisfroese@mailbox.org>
2024-04-07 18:30:23 +0200

    format, minor fix

--- helpers.py
+++ helpers.py
@@ -1,2 +1,9 @@
-relative = lambda landmark, shape: (int(landmark.x * shape[1]), int(landmark.y * shape[0]))
-relativeT = lambda landmark, shape: (int(landmark.x * shape[1]), int(landmark.y * shape[0]), 0)
+relative = lambda landmark, shape: (
+    int(landmark.x * shape[1]),
+    int(landmark.y * shape[0]),
+)
+relativeT = lambda landmark, shape: (
+    int(landmark.x * shape[1]),
+    int(landmark.y * shape[0]),
+    0,
+)
